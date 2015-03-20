bda5eaabed5a8b04fe19ed66731267945ece5691
Ma-rk <k.mark.kr@gmail.com>
2015-03-20 23:02:18 +0900

    arguments from shell

---
+++ ex02.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#arguments from shell
+
+echo ""
+echo "[arguments from shell]"
+echo "Total counts of arguments: $#"
+echo "First arg(\$0): $0"
+echo "Second arg(\$1): $1"
+echo "Third arg(\$2): $2"
+echo "Whole args:"
+echo $@
+echo ""
